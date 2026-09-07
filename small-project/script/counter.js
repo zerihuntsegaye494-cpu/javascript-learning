1ebbb178b3e4eaafafa2dd0d3095c5595feec2f2
Zerihun <zerihuntsegaye494@gmail.com>
2026-09-07 17:06:43 +0300

    making it more interactive

--- small-project/script/counter.js
+++ small-project/script/counter.js
@@ -1,44 +1,46 @@
+const display = document.querySelector(".counter-display");
+const increaseBtn = document.querySelector('.btn-increase');
+const decreaseBtn = document.querySelector('.btn-decrease');
+const resetBtn = document.querySelector('.btn-reset');
+const double = document.querySelector('.btn-double');
+
+
+
 let count=0;
 
-document.querySelector('#increaseBtn')
-.addEventListener('click',()=>{count+=1;
+increaseBtn.addEventListener('click',()=>{count+=1;
  updateDisplayColor()
 });
-
-
-document.querySelector('.btn-decrease')
-.addEventListener('click',()=>{count-=1;
+decreaseBtn.addEventListener('click',()=>{count-=1;
     updateDisplayColor()
 });
-
-
-
- document.querySelector('.btn-reset')
-.addEventListener('click',()=>{count=0;
+resetBtn.addEventListener('click',()=>{count=0;
  updateDisplayColor();
 });
 
+double.addEventListener('click',()=>{
+   count= count*2
+    updateDisplayColor();
+})
+
 function updateDisplayColor(){
 
-document.querySelector(".counter-display").textContent=count;
+    
 /*
 if (count > 0) {
-    document.querySelector(".counter-display").classList.add('positive');
+    display.classList.add('positive');
 } else if (count < 0) {
-    document.querySelector(".counter-display").classList.add('negative');
-}*/
+    display.classList.add('negative');
+*/
 
-if (count > 0) {
-    document.querySelector(".counter-display").classList.add('positive');
-    document.querySelector(".counter-display").classList.remove('negative');
-} else if (count < 0) {
-    document.querySelector(".counter-display").classList.remove('positive');
-    document.querySelector(".counter-display").classList.add('negative');
-} else {
-    // count === 0
-    document.querySelector(".counter-display").classList.remove('positive');
-    document.querySelector(".counter-display").classList.remove('negative');
-}
 
+    display.textContent = count;
+    display.classList.remove('positive', 'negative'); 
+    
+    if (count > 0) {
+        display.classList.add('positive');
+    } else if (count < 0) {
+        display.classList.add('negative');
+    }
 };
     
